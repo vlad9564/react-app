03f3ec3252e937739f6a01ed92507282fdf598c6
Ciobanu Vlad <ciobanu.vlad@cerner.com>
2019-03-28 09:39:37 +0200

    Updates

---
+++ precache-manifest.30feb4fce01848a96f6757d4cdf70cb4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "9ed022644c6f38b380ac",
+    "url": "/react-app/static/js/runtime~main.cbc86c42.js"
+  },
+  {
+    "revision": "94d8a29b228371c0826e",
+    "url": "/react-app/static/js/main.b4d9a5e7.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/react-app/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "94d8a29b228371c0826e",
+    "url": "/react-app/static/css/main.9ce7fe6b.chunk.css"
+  },
+  {
+    "revision": "b82b107e2e464085709265d1ee4fb673",
+    "url": "/react-app/index.html"
+  }
+];
